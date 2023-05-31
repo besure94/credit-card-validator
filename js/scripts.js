c33317633a2eb22d7898f882521c7e0b3ef94ac6
Brian Scherner <brianscherner@gmail.com>
2023-05-31 11:53:24 -0700

    Add for loop to doubleEveryOtherDigit function which will multiply every other number starting from the second digit by 2. Then take the doubled numbers that are two digits and add the two digits together. Create a new array with the two number digits added together.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 const creditCardNumber = "4102080860435620";
+// let creditCardNumberArray = [];
 
-function doubleEveryOtherDigit(doubledDigit) {
-	for (let index = 0; index )
+function doubleEveryOtherDigit(creditCardNumber) {
+	let creditCardNumberArray = [];
+	for(let index = 1; index < creditCardNumber.length; index += 2) {
+		let doubledNum = creditCardNumber[index] *= 2;
+		let doubledNumString = doubledNum.toString();
+		// if doubledNum is more than one digit, sum them together
+		if (doubledNumString.length > 1) {
+			doubledNum = parseInt(doubledNumString[0]) + parseInt(doubledNumString[1]);
+		}
+		creditCardNumberArray.push(parseInt(creditCardNumber[index -1]));
+		creditCardNumberArray.push(doubledNum);
+		console.log("Credit card number array: ", creditCardNumberArray);
+	}
+	return creditCardNumberArray;
 }
+
+function addDoubleDigitNumbers(number) {
+	const doubleDigitNumbers = ['16', '16' , '10'];
+}
+
+console.log("New credit card number: ", doubleEveryOtherDigit(creditCardNumber));
